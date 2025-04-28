e307f94fa9aea8a56567d88c334e61aaec8c9994
mike kimani <kimanimichael09@gmail.com>
2025-04-28 13:19:31 +0300

    created db tables

---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect('cake_sales.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS sales
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      date TEXT,
+                      region TEXT,
+                      heart_cakes INTEGER,
+                      coconut_cakes INTEGER,
+                      mobile_cakes INTEGER,
+                      block_cakes INTEGER,
+                      star_cakes INTEGER,
+                      queen_cakes INTEGER,
+                      sweet_cakes INTEGER)''')
+    conn.commit()
+    conn.close()
