a6f32583fec7619a80ba9608b760115864143a62
mike kimani <kimanimichael09@gmail.com>
2025-04-29 14:19:37 +0300

    updated a recommender.py module

--- recommender.py
+++ recommender.py
@@ -22,3 +22,17 @@ def get_recent_sales(region=None, days=30):
                 (region, since_date)
 
         )
+    else:
+        cursor.execute(
+            '''
+            SELECT ct.name, SUM(s.quantity)
+            FROM sales s 
+            JOIN cake_types ct ON s.cake_id = ct.id
+            JOIN regions r ON s.region_id = r.id
+            WHERE s.sale_date >= ?
+            GROUP BY ct.name
+            ORDER BY SUM (s.quantity) DESC
+            ''',
+                (since_date,)
+
+        )
